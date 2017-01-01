ec906a91ebc628c6ab46d8dfe0fdb18939469dfc
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-01-01 19:11:56 +0000

    adds background image to the home component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import './style.css';
+
 export default class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <div className="container">
+        <div>Home</div>
+      </div>
     );
   }
 }
