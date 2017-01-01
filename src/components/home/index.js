16a9778cdef1318637a3e7f8c9aa9dc57316a598
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-01-01 20:32:43 +0000

    adds buttons to the home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+import Navbar from '../../../lib/navbar';
+import TrainingMethods from '../../../lib/training_methods';
+
 import './style.css';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="container">
-        <div>Home</div>
+        <Navbar />
+        <TrainingMethods />
       </div>
     );
   }
---
+++ src/constants/training_methods.js
@@ -0,0 +1,16 @@
+const trainingMethods = [
+  'BODYBUILDING',
+  'CALISTHENICS',
+  'STRETCH',
+  'CROSSFIT',
+  'HIIT',
+  'CARDIO',
+  'POWER',
+  'SPORT',
+  'PREHAB',
+  'PHYSIO',
+  'OLYMPIC',
+  'YOGA',
+];
+
+export default trainingMethods;
