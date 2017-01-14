2531839cbc7af2c1ec0cf7053c0c1ae164a3ea94
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-01-14 20:37:12 +0000

    adds flow for trainers

--- src/components/trainer/index.js
+++ src/components/trainer/index.js
@@ -4,25 +4,25 @@ import Navbar from '../../../lib/navbar';
 
 import './style.css';
 
-export default class Athlete extends Component {
+export default class Trainer extends Component {
   constructor () {
     super();
     this.getTrainer = this.getTrainer.bind(this);
   }
   componentWillMount () {
     const trainer = this.props.location.pathname.split('/')[2];
-    console.log(trainer);
     this.getTrainer(trainer);
   }
   getTrainer (trainer) {
     this.props.getTrainerData(trainer);
   }
   render() {
-    const trainer = this.props.location.pathname.split('/')[2];
-    console.log(trainer);
+    const trainerData = this.props.trainer;
+    const videos = this.props.trainer[0] ? this.props.trainer[0].videos : [];
+    console.log('---------', videos, trainerData);
     return (
       <div>
-        <Navbar title="placeholder" />
+        <Navbar title={trainerData.name} />
       </div>
     );
   }
