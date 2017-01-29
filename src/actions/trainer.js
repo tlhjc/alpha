afcc2bc767725a8c64be6d133974cd010d2d8530
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-01-29 21:29:47 +0000

    sets up postgres

--- src/actions/trainer.js
+++ src/actions/trainer.js
@@ -1,13 +1,27 @@
 import { SET_TRAINER } from '../constants/action_types.js';
 import data from '../constants/trainer_data.js';
+import 'isomorphic-fetch';
 
 export const setTrainer = (trainerData) => { return {type: SET_TRAINER, trainerData}; };
 
 export const getTrainerData = (trainer) => {
   return (dispatch) => {
+    fetch('/trainer')
+      .then(function (response) {
+
+        console.log('response', response);
+        if (response.status >= 400) {
+           throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log('----', data);
+      })
     const trainerData = data.filter(data => {
       return data.instagram === trainer;
     });
+    console.log('*******', trainerData);
     dispatch(setTrainer(trainerData));
   }
 };
