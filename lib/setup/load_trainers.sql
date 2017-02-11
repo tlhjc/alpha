657e0562ca7873308c0d24a784b061100fc25360
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-02-11 13:54:48 +0000

    adds load trainers for physique

---
+++ lib/setup/load_trainers.sql
@@ -0,0 +1,16 @@
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('3', 'ulissesworld', 498981705, '3.2m', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22853885/2ab341b8-f059-11e6-80d5-abfbff0d59a3.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('4', 'simeonpanda', 17746208, '2.7m', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22853939/5c9fcfc4-f05a-11e6-9548-a103fd93bf84.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('5', 'jasonpostonpro', 30110648, '476k', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22853959/be88eba8-f05a-11e6-90a2-ff581d9f10f8.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('6', 'atrain_100', 24018213, '853k', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22854071/7bd4dc24-f05d-11e6-8270-b3fe53c8a553.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('7', 'mountaindog', 1162275307, '116k', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22854106/89e865b4-f05e-11e6-9926-c292c102ccf9.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('8', 'linnlowes', 53362084, '666k', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22854131/0d2f72fa-f05f-11e6-9994-a64ceb95ebfd.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('9', 'alexandrabring', 15432896, '655k', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22854162/c67b4b58-f05f-11e6-8a5d-e1cd7dff2e09.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('10', 'gracefituk', 278531736, '167k', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22854174/1285304a-f060-11e6-8a3f-2f1811bc582c.jpg');
--- src/actions/trainer.js
+++ src/actions/trainer.js
@@ -6,7 +6,7 @@ export const setTrainer = (trainerData) => { return {type: SET_TRAINER, trainerD
 
 export const getTrainerData = (trainer) => {
   return (dispatch) => {
-    fetch('/trainer?id=1')
+    fetch(`/trainer?handle=${trainer}`)
       .then(function (response) {
 
         if (response.status >= 400) {
