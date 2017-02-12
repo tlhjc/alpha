ef82d3c88e6282cd4e60bea008c9ae66acfabbf1
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-02-12 17:54:50 +0000

    adds olympic and crossfit trainers

--- lib/setup/load_trainers.sql
+++ lib/setup/load_trainers.sql
@@ -14,3 +14,31 @@ INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_me
 VALUES ('9', 'alexandrabring', 15432896, '655k', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22854162/c67b4b58-f05f-11e6-8a5d-e1cd7dff2e09.jpg');
 INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
 VALUES ('10', 'gracefituk', 278531736, '167k', 'physique', 'https://cloud.githubusercontent.com/assets/12450298/22854174/1285304a-f060-11e6-8a3f-2f1811bc582c.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('11', 'olychad', 465922680, '41.3k', 'olympic', 'https://cloud.githubusercontent.com/assets/12450298/22864306/c3aed394-f146-11e6-98a0-f7d33ba3d843.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('11', 'mattiecakesssss', 15729307, '393k', 'olympic', 'https://cloud.githubusercontent.com/assets/12450298/22864329/434b93a8-f147-11e6-840c-d49f2f8f5c99.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('12', 'kris10pope', 220228611, '63.4k', 'olympic', 'https://cloud.githubusercontent.com/assets/12450298/22864340/84f926e4-f147-11e6-9d2d-648dbb8b7a8c.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('13', 'jessicalucero9', 206575541, '61.5k', 'olympic', 'https://cloud.githubusercontent.com/assets/12450298/22864353/cee3298a-f147-11e6-9ebe-1d65297f392e.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('14', 'buffmendes', 625211204, '26.3k', 'olympic', 'https://cloud.githubusercontent.com/assets/12450298/22864372/1d8d8724-f148-11e6-8e46-3e643ea69316.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('15', 'aukhadov', 317731071, '62.5k', 'olympic', 'https://cloud.githubusercontent.com/assets/12450298/22864389/698a8320-f148-11e6-8aaa-ecf522850e00.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('16', 'ilyailyin_official', 779910041, '411k', 'olympic', 'https://cloud.githubusercontent.com/assets/12450298/22864402/c39e68ae-f148-11e6-8038-26c03c5362f9.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('17', 'hookgrip', 267650658, '343k', 'olympic', 'https://cloud.githubusercontent.com/assets/12450298/22864421/1e0a0fd2-f149-11e6-8a03-f43545b5f3fb.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('18', 'andrecrews', 710144982, '18.4k', 'crossfit', 'https://cloud.githubusercontent.com/assets/12450298/22864456/d885748c-f149-11e6-83c9-65f922902a8d.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('19', 'nohlsen', 18806680, '338k', 'crossfit', 'https://cloud.githubusercontent.com/assets/12450298/22864475/1ff5c6fa-f14a-11e6-82af-e38e37778fd6.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('20', 'mathewfras', 875276253, '556k', 'crossfit', 'https://cloud.githubusercontent.com/assets/12450298/22864496/7f965dfe-f14a-11e6-949e-37ce4e55fbda.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('21', 'katrintanja', 22755425, '681k', 'crossfit', 'https://cloud.githubusercontent.com/assets/12450298/22864532/240a305e-f14b-11e6-9ed0-82c6f47b9a23.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('22', 'sarasigmunds', 282919004, '540k', 'crossfit', 'https://cloud.githubusercontent.com/assets/12450298/22864581/e6486316-f14b-11e6-8476-400cedd9ed01.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('23', 'anniethorisdottir', 260510081, '554k', 'crossfit', 'https://cloud.githubusercontent.com/assets/12450298/22864594/3cc086ba-f14c-11e6-9478-03a64671ca6b.jpg');
