51df66e47831b4c4c7f598b1251825339504ea45
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-02-12 20:40:21 +0000

    finishes off data addition

--- lib/setup/load_trainers.sql
+++ lib/setup/load_trainers.sql
@@ -42,3 +42,47 @@ INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_me
 VALUES ('22', 'sarasigmunds', 282919004, '540k', 'crossfit', 'https://cloud.githubusercontent.com/assets/12450298/22864581/e6486316-f14b-11e6-8476-400cedd9ed01.jpg');
 INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
 VALUES ('23', 'anniethorisdottir', 260510081, '554k', 'crossfit', 'https://cloud.githubusercontent.com/assets/12450298/22864594/3cc086ba-f14c-11e6-9478-03a64671ca6b.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('24', 'thebodycoach', 42228698, '1.6m', 'hiit', 'https://cloud.githubusercontent.com/assets/12450298/22864969/c0fbc6a0-f152-11e6-9dd3-3c542342c881.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('25', 'stef_fit_', 3577018212, '121k', 'hiit', 'https://cloud.githubusercontent.com/assets/12450298/22865034/bc804208-f153-11e6-934c-93f204db9bde.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('26', 'thefoodmedic', 358201416, '107k', 'hiit', 'https://cloud.githubusercontent.com/assets/12450298/22865044/084b3468-f154-11e6-81e2-5aebab680461.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('27', 'twobadbodies', 581534816, '340k', 'hiit', 'https://cloud.githubusercontent.com/assets/12450298/22865062/48928dbe-f154-11e6-9ea7-87144befd269.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('28', 'bradleysimmonds', 178544238, '147k', 'hiit', 'https://cloud.githubusercontent.com/assets/12450298/22865077/9081b532-f154-11e6-9d85-570c5420ff1e.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('29', 'sjanaelise', 624360091, '1.2m', 'yoga', 'https://cloud.githubusercontent.com/assets/12450298/22865089/de6a5e34-f154-11e6-8b51-b1f50508e6fa.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('30', 'bryceyoga', 624360091, '155k', 'yoga', 'https://cloud.githubusercontent.com/assets/12450298/22865110/2ab5f532-f155-11e6-82f7-9ad49707612d.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('31', 'beachyogagirl', 9614635, '1m', 'yoga', 'https://cloud.githubusercontent.com/assets/12450298/22865124/728b27c4-f155-11e6-980a-6913a9b69962.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('32', 'tarastiles', 8603910, '137k', 'yoga', 'https://cloud.githubusercontent.com/assets/12450298/22865137/c402c0a8-f155-11e6-83d1-7c4facc3e659.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('33', 'aris_inmotion', 405638381, '67.3k', 'yoga', 'https://cloud.githubusercontent.com/assets/12450298/22865176/4caddfdc-f156-11e6-995b-d3f890097608.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('34', 'cj_fitlegend', 49615342, '14.8k', 'strength and conditioning', 'https://cloud.githubusercontent.com/assets/12450298/22865241/a650b6a8-f157-11e6-8382-6facc4c1843f.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('35', 'coachpjnes24', 201267098, '4k', 'strength and conditioning', 'https://cloud.githubusercontent.com/assets/12450298/22865565/b538eb8e-f15e-11e6-9967-82a25ff618f7.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('36', 'athleanx', 1050236256, '67.5k', 'strength and conditioning', 'https://cloud.githubusercontent.com/assets/12450298/22865581/23af2bd2-f15f-11e6-8864-bb296d3de054.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('37', 'speed__doctor', 1950326289, '327k', 'strength and conditioning', 'https://cloud.githubusercontent.com/assets/12450298/22865594/7594e73e-f15f-11e6-8e6e-d9317eedb1b0.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('38', 'portal.ido', 2314126316, '333k', 'calisthenics', 'https://cloud.githubusercontent.com/assets/12450298/22865617/cf47a50a-f15f-11e6-9889-48fa5dd9f1a8.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('39', 'rawcalisthenics', 19009605, '161k', 'calisthenics', 'https://cloud.githubusercontent.com/assets/12450298/22865629/1a1b6616-f160-11e6-8b1d-397d65e46970.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('40', 'al_kavadlo', 1628132086, '17.2k', 'calisthenics', 'https://cloud.githubusercontent.com/assets/12450298/22865662/da2d7340-f160-11e6-8717-159ac33e2846.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('41', 'frank_medrano', 196238381, '867k', 'calisthenics', 'https://cloud.githubusercontent.com/assets/12450298/22865680/339f0308-f161-11e6-959c-c6e669d9ce17.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('42', 'barstarzz', 43810255, '541k', 'calisthenics', 'https://cloud.githubusercontent.com/assets/12450298/22865695/70eff4f6-f161-11e6-9295-b9ee248360f5.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('43', 'barbrothersofficial', 300135572, '320k', 'calisthenics', 'https://cloud.githubusercontent.com/assets/12450298/22865717/ca3b0eba-f161-11e6-95ff-ed87acb3bf9f.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('44', 'theprehabguys', 2321056392, '88.2k', 'physiotherapy', 'https://cloud.githubusercontent.com/assets/12450298/22865752/569026d4-f162-11e6-88b9-d6eed5f98a5b.jpg');
+INSERT INTO trainers (id, instagram_handle, instagram_id, followers, training_method, profile_image_url)
+VALUES ('45', 'victoryperformancept', 1733469808, '1.1k', 'physiotherapy', 'https://cloud.githubusercontent.com/assets/12450298/22865813/4d36c0ce-f163-11e6-8f8e-fd72336041a0.jpg');
